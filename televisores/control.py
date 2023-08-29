2a45a8132321446581024476a4d2a5e2c43c6ffb
santix-l <137665289+santix-l@users.noreply.github.com>
2023-08-28 21:34:38 -0500

    Update control.py

---
+++ televisores/control.py
@@ -0,0 +1,55 @@
+from televisores.tv import TV
+
+class Control:
+
+    def __init__(self):
+        
+        self._tv = None
+
+    def canalUp(self):
+
+        self._tv.canalUp()
+    
+    def canalDown(self):
+
+       self._tv.canalDown()
+    
+    def volumenUp(self):
+
+        self._tv.volumenUp()
+    
+    def volumenDown(self):
+
+        self._tv.volumenDown()
+    
+    def turnOff(self):
+
+        self._tv.turnOff()
+    
+    def turnOn(self):
+
+        self._tv.turnOn()
+    
+    def setCanal(self):
+
+        self._tv.setCanal()
+    
+    def setVolumen(self):
+
+        self._tv.setVolumen()
+
+    def enlazar(self, tv):
+
+        self._tv = tv
+
+        tv.setControl(self)
+    
+    def setTV(self, t):
+
+        self._tv = t
+
+    def getTV(self):
+        
+        return self._tv
+
+    
